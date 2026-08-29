9aa07473e9a843c893e85051d2b332f37521e748
Carey McGilliard NOAA <carey.mcgilliard@noaa.gov>
2026-08-28 20:48:23 -0700

    changed timeseries plot to go to 0 for y axes

--- R/plot_timeseries.R
+++ R/plot_timeseries.R
@@ -2,20 +2,34 @@
 
 #' Plot time-series results from multiple Stock Synthesis 3 assessment models with uncertainty
 #'
-#' @param data.t
-#' @param showlegend
+#' @param data.t time series data as a data frame or tibble with Year and Value columns
+#' @param showlegend true or false
+#' @param xlabel the label for the x axis
+#' @param ylabel the label for the y axis
+#' @param color_option used within scale_color_viridis_d for changing the color theme
+#' @param color_direction switch when needed to show the data better
 #'
 #' @return
 #' @export
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 geom_line
+#' @importFrom ggplot2 geom_ribbon
+#' @importFrom ggplot2 labeller
+#' @importFrom ggplot2 theme
+#' @importFrom viridis scale_color_viridis
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 guides
+#' @importFrom ggplot2 guide_legend
 #'
 #' @examples
 #' plot_timeseries(data.t = rec.t,xlabel= "Year",ylabel = "Age 0 (thousands)",showlegend = "none")
 #'
 plot_timeseries<-function(data.t,xlabel,ylabel,showlegend,color_option = "viridis",color_direction = 1) {
 
-  tsplot<-ggplot(data = data.t,aes(x = as.numeric(Year),y=Value)) +
+  tsplot<-ggplot2::ggplot(data = data.t,aes(x = as.numeric(Year),y=Value)) +
     geom_line(aes(color = factor(Model)),linewidth = 1.2,alpha = 1) +
-    geom_ribbon(aes(ymin = lb,ymax = ub,fill = factor(Model)),alpha =0.2,show.legend = FALSE) +
+    geom_ribbon(aes(ymin = 0,ymax = ub,fill = factor(Model)),alpha =0.2,show.legend = FALSE) +
     labs(x=xlabel,y = ylabel,color = "Model") +
     scale_color_viridis_d(aesthetics = c("colour","fill"), option = color_option,direction = color_direction) +
     if (showlegend == "none") {
